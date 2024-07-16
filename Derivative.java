4f6d66f615847a621e79b74db18446e01cffafdd
sanskarkanade <kanadesanskar79@gmail.com>
2024-07-16 20:02:20 +0530

    Update Derivative.java

--- Derivative.java
+++ Derivative.java
@@ -4,7 +4,6 @@ class Derivative
 {
     public static void main(String[] args) 
     {  
-        System.out.println("hello");
        String s1=new String("a");
        String s2=new String("sinx");
        String s3=new String("cosx");
